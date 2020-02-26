ffb05e3d85205240a4f984c63a729b2a62763894
neha doodi and Tiffany Siu <neha@>
2020-02-26 10:19:55 -0800

    add animal properties and functions, add main UI functions

--- Animal.cs
+++ Animal.cs
@@ -1,8 +1,82 @@
 using System;
 
-namespace Animals {
-  public class Animals
+namespace AnimalTracker {
+  public class Animal
   {
+    private string _species;
+    private int _trackingNumber;
+    private string _habitat;
+    private string _dietCategory;
 
+    private static int _currentNumber = -1;
+    public Animal (string species, string habitat, string diet)
+    {
+      _species = species;
+      _trackingNumber = SetTrackingNumber();
+      _habitat = habitat;
+      _dietCategory = diet;
+    }
+
+    public string GetSpecies()
+    {
+      return _species;
+    }
+
+    public int GetTrackingNumber()
+    {
+      return _trackingNumber;
+    }
+
+    public int SetTrackingNumber()
+    {
+      _currentNumber = _currentNumber +1;
+      return _currentNumber;
+    }
+    public string GetHabitat()
+    {
+      return _habitat;
+    }
+
+    public void SetHabitat(string newHabitat)
+    {
+      _habitat = newHabitat;
+    }
+
+    public string GetDietCategory()
+    {
+      return _dietCategory;
+    }
+
+    public static string ParkArea (string area)
+    {
+      return "Animals tracked near " + area + " in the wildlife park.";
+    }
+
+    public bool CheckSpecies (string species)
+    {
+      if (_species == species){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public bool CheckHabitat (string habitat)
+    {
+      if (_habitat == habitat){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public bool CheckDiet (string diet)
+    {
+      if (_dietCategory == diet){
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,79 @@
 using System;
-using Animals;
+using AnimalTracker;
+using System.Collections.Generic;
 
 public class Program {
   static void Main()
   {
+    // Create initial animals for list
+    Animal lion = new Animal("lion", "savannah", "carnivore");
+    Animal crocodile = new Animal("crocodile", "swamp", "carnivore");
+    Animal giraffe = new Animal("giraffe", "savannah", "herbivore");
+    List<Animal> Animals = new List<Animal>() {lion, crocodile, giraffe};
 
+    // Ask for initial input
+    Console.WriteLine("What do you want to do (Add/Show/Search)");
+    string input = Console.ReadLine();
+
+    if (input == "Add"){
+      Console.WriteLine("What species of animal do you want to add?");
+      string speciesToAdd = Console.ReadLine();
+      Console.WriteLine("What is their habitat?");
+      string habitatToAdd = Console.ReadLine();
+      Console.WriteLine("What is their diet category? (carnivore, herbivore, omnivore)");
+      string dietToAdd = Console.ReadLine();
+
+      Animal animalToAdd = new Animal(speciesToAdd, habitatToAdd, dietToAdd);
+      Animals.Add(animalToAdd);
+      Console.WriteLine("Animal added! ID: " + Animals[animalToAdd.GetTrackingNumber()].GetTrackingNumber() + " Species: " + Animals[animalToAdd.GetTrackingNumber()].GetSpecies() + " Habitat: " + Animals[animalToAdd.GetTrackingNumber()].GetHabitat() + " Diet Category: " + Animals[animalToAdd.GetTrackingNumber()].GetDietCategory());
+
+      PrintAllAnimals(Animals);
+    } else if (input == "Show"){
+      PrintAllAnimals(Animals);
+    } else if (input == "Search"){
+      Console.WriteLine("What to search by? (species/habitat/diet)");
+      string searchCriteria = Console.ReadLine();
+      List<Animal> AnimalSearch = new List<Animal>(0);
+
+      if (searchCriteria == "species"){
+        Console.WriteLine("What species are you looking for?");
+        string speciesToSearch = Console.ReadLine();
+        foreach (Animal animalChecking in Animals)
+        {
+          if (animalChecking.CheckSpecies(speciesToSearch)){
+            AnimalSearch.Add(animalChecking);
+          }
+        }
+      } else if (searchCriteria == "habitat"){
+        Console.WriteLine("What habitat are you looking for?");
+        string habitatToSearch = Console.ReadLine();
+        foreach (Animal animalChecking in Animals)
+        {
+          if (animalChecking.CheckHabitat(habitatToSearch)){
+            AnimalSearch.Add(animalChecking);
+          }
+        }
+      } else if (searchCriteria == "diet"){
+        Console.WriteLine("What diet category are you looking for?");
+        string dietToSearch = Console.ReadLine();
+        foreach (Animal animalChecking in Animals)
+        {
+          if (animalChecking.CheckDiet(dietToSearch)){
+            AnimalSearch.Add(animalChecking);
+          }
+        }
+      }
+      PrintAllAnimals(AnimalSearch);
+    }
+  }
+
+  public static void PrintAllAnimals(List<Animal> Animals)
+  {
+    Console.WriteLine("Animals in Program:");
+    Console.WriteLine(Animal.ParkArea("the Safari zone"));
+      foreach(Animal anAnimal in Animals)
+      {
+        Console.WriteLine("ID: " + anAnimal.GetTrackingNumber() + " Species: " + anAnimal.GetSpecies() + " Habitat: " + anAnimal.GetSpecies() + " Diet Category: " + anAnimal.GetDietCategory());
+      }
   }
 }
